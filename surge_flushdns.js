ff60dff7932dc9911647eadd93633a0f60f6075f
hjl1201 <hjl120119@gmail.com>
2022-03-18 01:24:00 +0800

    panel

--- surge_flushdns.js
+++ surge_flushdns.js
@@ -35,10 +35,10 @@ let seconds=Math.round(leave3/1000)
 if(days==0){
 
 	if(hours==0){
-	if(minutes==0)return(`${seconds}:`);
-	return(`${minutes}:${seconds}:`)
+	if(minutes==0)return(`${seconds}`);
+	return(`${minutes}:${seconds}`)
 	}
-	return(`${hours}:${minutes}:${seconds}:`)
+	return(`${hours}:${minutes}:${seconds}`)
 	}else {
 	return(`${days}:${hours}:${minutes}:`)
 	}
