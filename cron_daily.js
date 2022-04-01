dabf85e53643c92365e448bd0f6b6c1add05f32f
hjl1201 <hjl120119@gmail.com>
2022-04-01 12:07:03 +0800

    cron_daily.js

--- cron_daily.js
+++ cron_daily.js
@@ -1,82 +1,37 @@
-/*
-README：https://github.com/yichahucha/surge/tree/master
-每日蚂蚁收能量提醒（corn "11 7 * * *" 每天7:11）+ 每日壹句（有道词典）+ 点击通知跳转支付宝蚂蚁森林页面
-*/
+const $ = new compatibility()
+/*const wait_family = $.read('Alipay_wait_family') * 1000 || 5000
+const wait_mayi = $.read('Alipay_wait_mayi') * 1000 || 5000
+const point = "alipays://platformapi/startapp?appId=20000160&url=/www/pointSignIn.html"
+const familypoint = "alipays://platformapi/startapp?appId=2019052365379124"*/
+const mayi = "alipay://platformapi/startapp?appId=60000002"
 
-const $tool = new Tool()
-$tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
-    let obj = JSON.parse(data);
-    let date = new Date();
-    let isAM = date.getHours() < 12 ? true : false;
-    let title = '每日' + (isAM ? ' 壹句' : ' 壹句') + (isAM ? ' ☀️' : ' 🌙');
-    let subtitle = '';
-    let scheme = 'alipay://platformapi/startapp?appId=60000002';
-    let content = "收能量啦👉"+scheme;
-    let option = {"open-url" : scheme};
-    if (!error) {
-        if (obj && obj.length > 1) {
-            let yi = obj[1];
-            content = yi.title + '\n' + yi.summary + '\n\n' + content;
-            option["media-url"] = yi.image[0];
-        }
-    }
-    $tool.notify(title, subtitle, content, option);
-    $done();
-})
+let delay = function(s){
+    return new Promise(function(resolve,reject){
+        setTimeout(resolve,s)
+    })
+}
 
-function Tool() {
-    _node = (() => {
-        if (typeof require == "function") {
-            const request = require('request')
-            return ({ request })
-        } else {
-            return (null)
-        }
-    })()
-    _isSurge = typeof $httpClient != "undefined"
+delay().then(function(){
+    /*$.notify("支付宝", "", "领积分啦", point)
+    return delay(wait_family)
+}).then(function(){
+    $.notify("支付宝", "", "领家庭积分啦", familypoint)
+    return delay(wait_mayi)
+}).then(function(){*/
+    $.notify("支付宝", "", "收能量啦", mayi)
+}).finally(() => $done())
+
+function compatibility() {
     _isQuanX = typeof $task != "undefined"
-    this.isSurge = _isSurge
-    this.isQuanX = _isQuanX
-    this.isResponse = typeof $response != "undefined"
-    this.notify = (title, subtitle, message, option) => {
-        if (_isQuanX) $notify(title, subtitle, message, option)
-        if (_isSurge) $notification.post(title, subtitle, message, option["open-url"])
-        if (_node) console.log(JSON.stringify({ title, subtitle, message }));
-    }
-    this.write = (value, key) => {
-        if (_isQuanX) return $prefs.setValueForKey(value, key)
-        if (_isSurge) return $persistentStore.write(value, key)
-    }
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
     this.read = (key) => {
         if (_isQuanX) return $prefs.valueForKey(key)
-        if (_isSurge) return $persistentStore.read(key)
+        if (_isLoon) return $persistentStore.read(key)
     }
-    this.get = (options, callback) => {
-        if (_isQuanX) {
-            if (typeof options == "string") options = { url: options }
-            options["method"] = "GET"
-            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-        }
-        if (_isSurge) $httpClient.get(options, (error, response, body) => { callback(error, _status(response), body) })
-        if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
-    }
-    this.post = (options, callback) => {
-        if (_isQuanX) {
-            if (typeof options == "string") options = { url: options }
-            options["method"] = "POST"
-            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-        }
-        if (_isSurge) $httpClient.post(options, (error, response, body) => { callback(error, _status(response), body) })
-        if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
-    }
-    _status = (response) => {
-        if (response) {
-            if (response.status) {
-                response["statusCode"] = response.status
-            } else if (response.statusCode) {
-                response["status"] = response.statusCode
-            }
-        }
-        return response
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
     }
 }
